97b8e32d03b113e6ac8565a0b8db358f84c5cc23
Thiago Oliveira <Thiago.Oliveira2@fit-tecnologia.org.br>
2023-09-29 14:55:37 -0400

    add auth controller in routes correctly

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const routes = express.Router();
 const authMiddleware = require('./middlewares/auth');
-const AuthController = require('./controllers/authController');
+const AuthController = require('./controllers/AuthController');
+const ProjectController = require('./controllers/projectController');
 
 routes.post("/register", AuthController.register);
 routes.post("/login", AuthController.login);
 routes.get("/profile", authMiddleware, AuthController.profile);
 routes.post("/forgot_password", AuthController.forgot_password);
 routes.post("/reset_password", AuthController.reset_password);
+
+routes.get("/projects", ProjectController.index);
+routes.get("/projects/:id", ProjectController.show);
+routes.post("/projects", ProjectController.create);
+routes.put("/projects/:id", ProjectController.update);
+routes.delete("/projects/:id", ProjectController.destroy);
 
 module.exports = routes;
